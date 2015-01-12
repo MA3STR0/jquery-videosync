96edf8807ae20fe9ca1cd2f18803f3626f825cc7
Anton <git@maestr0.com>
2015-01-12 17:15:07 +0100

    Add videosync.js

---
+++ jquery.videosync.js
@@ -0,0 +1,22 @@
+$('video').each( function() {
+    var video = this;
+    var nodes = [];
+    var time = 0;
+      $('.videosync').not('video').each(function(){
+          var $this = $(this);
+          nodes.push({
+              start: this.getAttribute('data-videosync-start'),
+              end: this.getAttribute('data-videosync-end'),
+              element: $this,
+          });
+      });
+    video.addEventListener('timeupdate',function(){
+        time = parseInt(video.currentTime);
+        for (var i = 0; i < nodes.length; i++) {
+            var node = nodes[i];
+            if (time >= node.start) {
+                node.element.show();
+            }
+        }
+    }, false);
+});
