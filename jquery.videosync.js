774aee253f92f9e46bd7515b537827e47d2024bf
Anton <git@maestr0.com>
2015-05-25 12:02:19 +0200

    Add vs-in and vs-out attributes

--- jquery.videosync.js
+++ jquery.videosync.js
@@ -1,19 +1,22 @@
 /*! jQuery VideoSync
  *  https://github.com/MA3STR0/jquery-videosync
- *  Shows or hides page content based on video playback time */
+ *  Manipulates page content based on video playback time */
 (function($) {
     $.fn.videosync = function() {
         return this.each( function() {
+            var classIn = 'vs-in';
+            var classOut = 'vs-out';
             var video = this;
             var nodes = [];
             var lastTime = -1;
             var time;
-              $('.videosync').each(function(){
+              $('.vs').each(function(){
                   var $this = $(this);
                   nodes.push({
-                      start: parseFloat($this.data('videosync-start')),
-                      end: parseFloat($this.data('videosync-end')),
-                      class: $this.data('videosync-class'),
+                      start: parseFloat($this.data('vs-in-time')),
+                      end: parseFloat($this.data('vs-out-time')),
+                      classIn: $this.data('vs-in-class'),
+                      classOut: $this.data('vs-out-class'),
                       element: $this
                   });
               });
@@ -25,14 +28,20 @@
                 lastTime = time;
                 for (var i = 0; i < nodes.length; i++) {
                     var node = nodes[i];
-                    if ((!node.element.hasClass(node.class)) && (time >= node.start) && (node.end ? time < node.end : true)) {
-                        node.element.addClass(node.class);
-                    } else if ((node.element.hasClass(node.class)) && ((time < node.start) || (node.end ? time >= node.end : false))) {
-                        node.element.removeClass(node.class);
+                    if ((!node.element.hasClass(classIn)) && (time >= node.start) && (node.end ? time < node.end : true)) {
+                        node.element.removeClass(classOut);
+                        node.element.removeClass(node.classOut);
+                        node.element.addClass(node.classIn);
+                        node.element.addClass(classIn);
+                    } else if ((node.element.hasClass(classIn)) && ((time < node.start) || (node.end ? time >= node.end : false))) {
+                        node.element.removeClass(node.classIn);
+                        node.element.removeClass(classIn);
+                        node.element.addClass(classOut);
+                        node.element.addClass(node.classOut);
                     }
                 }
             }, false);
         });
     };
-    $('video.videosync-source').videosync();
+    $('video.vs-source').videosync();
 }(jQuery));
