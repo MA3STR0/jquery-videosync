d35807525977aab6626e16499c2efcf9132fcf7f
Anton <git@maestr0.com>
2015-05-25 09:55:46 +0200

    Add helper scripts

---
+++ build.sh
@@ -0,0 +1,2 @@
+uglifyjs jquery.videosync.js  --source-map jquery.videosync.min.js.map  > jquery.videosync.min.js
+cp jquery.videosync.min.js demo/jquery.videosync.min.js
---
+++ push_demo.sh
@@ -0,0 +1,1 @@
+git subtree push --prefix demo origin gh-pages
